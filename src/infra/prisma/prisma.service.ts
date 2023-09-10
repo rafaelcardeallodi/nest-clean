aa1695c59bfb844dacbd610024815da462ad3a9e
Rafael Lodi <rafaelcardeallodi@hotmail>
2023-09-10 20:10:32 -0300

    Criando camada de Infraestrutura

--- src/infra/prisma/prisma.service.ts
+++ src/infra/prisma/prisma.service.ts
@@ -7,9 +7,7 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   constructor() {
-    super({
-      log: ['info', 'warn'],
-    })
+    super()
   }
 
   onModuleInit() {
