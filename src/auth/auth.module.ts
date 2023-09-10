abed46e368f3f97b64194b8ec91c395e192e15ae
Rafael Lodi <rafaelcardeallodi@hotmail>
2023-09-10 13:21:47 -0300

    Gerando token JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,12 +9,15 @@ import { Env } from 'src/env'
     PassportModule,
     JwtModule.registerAsync({
       inject: [ConfigService],
+      global: true,
       useFactory: (configService: ConfigService<Env, true>) => {
-        const secret = configService.get('JWT_SECRET', { infer: true })
+        const privateKey = configService.get('JWT_PRIVATE_KEY', { infer: true })
+        const publicKey = configService.get('JWT_PUBLIC_KEY', { infer: true })
 
         return {
-          secret,
-          signOptions: { expiresIn: '1d' },
+          signOptions: { algorithm: 'RS256' },
+          privateKey: Buffer.from(privateKey, 'base64'),
+          publicKey: Buffer.from(publicKey, 'base64'),
         }
       },
     }),
